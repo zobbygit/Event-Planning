8f1209f7ca2285e770a5b188d609b3c57d536917
Zohaib <164843889+zobbygit@users.noreply.github.com>
2025-10-19 20:43:14 +0530

    Implement CORS middleware with allowed origins
    Added CORS support with allowed origins for local and deployed environments.

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,26 @@
 import app from "./app.js";
+import dotenv from "dotenv";
+import cors from "cors";
+
+
+dotenv.config()
+const allowedOrigins = [
+  'http://localhost:5173', // For local development
+  process.env.FRONTEND_URL, // For the deployed site
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 
 app.listen(process.env.PORT , () => {
     console.log(`Server Listing on Port ${process.env.PORT}`);
