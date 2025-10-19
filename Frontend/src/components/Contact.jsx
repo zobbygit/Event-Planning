be6e51343561eb9e2e55d184970a10e82914ec44
Zohaib <164843889+zobbygit@users.noreply.github.com>
2025-10-19 20:47:33 +0530

    Update API endpoint for sending messages

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     e.preventDefault();
     await axios
       .post(
-        "http://localhost:4000/api/v1/message/send",
+        "https://event-planning-backend-goce.onrender.com/api/v1/message/send",
         { name, email, subject, message },
         { withCredentials: true, headers: { "Content-Type": "application/json" } }
       )
